276d18cbcab1f86189b16a221a3157e11caed16b
Prince Saini <princesaini0144@gmail.com>
2025-01-29 20:24:32 +0530

    Initial Commit

---
+++ force-app/main/default/lwc/movieSearch/movieSearch.js
@@ -0,0 +1,45 @@
+import { LightningElement } from 'lwc';
+const DELAY = 300;
+export default class MovieSearch extends LightningElement {
+    selectedType = "";
+    selectedSearch = "";
+    loading = false;
+    selectedPageNo = "1"; 
+    delayTimeout;
+
+    get typeOptions(){
+        return [
+            {label : 'None', value : ''},
+            {label : 'Movie' , value : 'movie'},
+            {label : 'Series',  value : 'series'},
+            {label : 'Episode', value : 'episode'}
+        ];
+    }
+
+    handleChange(event){
+        let {name, value} = event.target;
+        this.loading = true;
+        if(name === 'type'){
+            this.selectedType = value;
+        }else if(name === 'search'){
+            this.selectedSearch = value;
+        }else if(name === 'pageno'){
+            this.selectedPageNo = value;
+        }
+
+        //debouncing
+        clearTimeout(this.delayTimeout);
+        this.delayTimeout = setTimeout(()=> {
+            this.searchMovie();
+        } , DELAY);
+    }
+
+    //this method will search for the entered movie
+    async searchMovie(){
+        const url = `https://www.omdbapi.com/?s=${this.selectedSearch}&type=${this.selectedType}&page=${this.selectedPageNo}&apikey=28122ba`;
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log("Movie Search Output", data);
+    }
+
+}
